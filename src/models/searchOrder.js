73a46658d76f7352418fa1eed98243d541f5b73f
Julian Baños y Rivas <julian.b@sirena.app>
2020-02-19 14:02:22 -0300

    validate searchData in searchOrder schema

--- src/models/searchOrder.js
+++ src/models/searchOrder.js
@@ -1,18 +1,31 @@
 const mongoose = require('mongoose');
+const searchData = require('../../resources/search-data.json');
+const searchDataKeys = Object.keys(searchData).sort();
 const statusNames = {
   received: 'received',
   processing: 'processing',
   fulfilled: 'fulfilled',
   failed: 'failed'
 };
+const searchStatus = Object.values(statusNames);
 
 const searchOrderSchema = mongoose.Schema(
   {
-    searchData: { type: Object, required: true },
+    searchData: {
+      type: Object,
+      required: true,
+      validate: {
+        validator: function(v) {
+          const vKeys = Object.keys(v).sort();
+          return JSON.stringify(vKeys) === JSON.stringify(searchDataKeys);
+        },
+        message: props => `${props.value} is not a valid search data`
+      }
+    },
     orderStatus: {
       type: String,
       default: statusNames.received,
-      enum: Object.values(statusNames)
+      enum: searchStatus
     },
     products: { type: Array, default: [] }
   },
--- test/endpoints.test.js
+++ test/endpoints.test.js
@@ -3,7 +3,8 @@ const should = chai.should();
 const expect = chai.expect;
 const chaiHttp = require('chai-http');
 const server = require('../server');
-const searchOrder = require('./resources/search-data.json');
+const searchOrder = require('../resources/search-data.json');
+const invalidSearchOrder = require('./resources/invalidSearchOrder.json');
 
 chai.use(chaiHttp);
 describe('GET /', () => {
@@ -38,6 +39,17 @@ describe('POST /api/product/search', () => {
         expect(err).to.be.null;
         expect(res.status).to.equal(201);
         expect(res.body.status).to.be.equal('Successful');
+        done();
+      });
+  });
+  it('should return status 400', done => {
+    chai
+      .request(server)
+      .post('/api/product/search')
+      .send(invalidSearchOrder)
+      .end((err, res) => {
+        expect(err).not.to.be.undefined;
+        expect(res.status).to.equal(400);
         done();
       });
   });
