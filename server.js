c5b6cfaaca83033396c6fa91d0d8f584d2ae1ab5
Julian Baños y Rivas <julian.b@sirena.app>
2020-02-19 03:47:34 -0300

    fix

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const http = require('http');
 const app = require('./src/app');
 const server = http.createServer(app);
 
-server.listen(process.env.port || '3000');
+server.listen(process.env.PORT || '3000');
 module.exports = server;
