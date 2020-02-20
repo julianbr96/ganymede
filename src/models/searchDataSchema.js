82e818f5f136e5e88945810725c80fdb3f74a590
Julian Baños y Rivas <julian.b@sirena.app>
2020-02-20 12:35:10 -0300

    fix id generation for search data

--- src/models/searchDataSchema.js
+++ src/models/searchDataSchema.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
-const searchDataSchema = mongoose.Schema({
-  query: { type: String, required: true },
-  provider: { type: String, required: true },
-  options: { type: Object, required: true },
-  callbackUrl: { type: String, required: true }
-});
+const searchDataSchema = mongoose.Schema(
+  {
+    query: { type: String, required: true },
+    provider: { type: String, required: true },
+    options: { type: Object, required: true },
+    callbackUrl: { type: String, required: true }
+  },
+  { _id: false }
+);
 
 module.exports = searchDataSchema;
